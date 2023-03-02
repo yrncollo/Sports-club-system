5a73134842a429a2dd921cd02bd058bcfeb416f6
yrncollo <ngenocollins599@gmail.com>
2023-03-02 17:21:44 +0300

    added registration form

--- main.cpp
+++ main.cpp
@@ -1,5 +1,21 @@
 #include <iostream>
 #include <string>
+#include <sys/types.h>
+
+struct registert
+{
+
+		std::string fullName;
+		std::string gender;
+		std::string nextOfKin;
+		std::string dOB;
+		std::string contact;
+		std::string subCounty;
+		std::string games;
+		std::string weight;
+		std::string height;
+		std::string specialNeeds;
+};
 
 class maringo
 {
@@ -12,6 +28,7 @@ class maringo
 		void welcome();
 		void school();
 		void religious();
+		void registration();
 };
 
 void maringo::welcome()
@@ -21,6 +38,16 @@ void maringo::welcome()
 	std::cout << "\t\t|                                               |" << "\n"; 
 	std::cout << "\t\t|     WELCOME TO MARINGO SPORTS CLUB SYSTEM     |" << "\n";
 	std::cout << "\t\t|                                               |" << "\n"; 
+	std::cout << "\t\t| Maringo offers the following games:           |" << "\n"; 
+	std::cout << "\t\t| \t 1) Swimming \t 2) Hockey              |" << "\n"; 
+	std::cout << "\t\t| \t 3) Lawn tennis  4) Table tennis        |" << "\n"; 
+	std::cout << "\t\t| \t 5) Darts \t 6) Badminton           |" << "\n"; 
+	std::cout << "\t\t| \t 7) Volleyball \t 8) Basketball          |" << "\n"; 
+	std::cout << "\t\t| \t 9) Netball \t 10) Football           |" << "\n"; 
+	std::cout << "\t\t| \t 11) Baseball \t 12) Rugby              |" << "\n"; 
+	std::cout << "\t\t| \t 13) Pool \t 14) Chess              |" << "\n"; 
+	std::cout << "\t\t| \t 15) Draft                              |" << "\n"; 
+	std::cout << "\t\t|-----------------------------------------------|" << "\n\n";
 	std::cout << "\t\t|-----------------------------------------------|" << "\n";
 	std::cout << "\t\t|\t Where are you registering from?:       |" << "\n"; 
 	std::cout << "\t\t|-----------------------------------------------|" << "\n";
@@ -86,11 +113,39 @@ void maringo::religious()
 
 }
 
+void maringo::registration()
+{
+
+	registert *ptr, reg; //Create a pointer variable *ptr and normal variable reg of type registert.
+	ptr = &reg; //Store the address of variable reg in our pointer variable.
+	std::cout << "\n Fill the form bellow: \n";
+	std::cout << "\tFull name: ";
+	std::cin >> (*ptr).fullName;
+	std::cout << "\tGender: ";
+	std::cin >> (*ptr).gender ;
+	std::cout << "\tNext of kin: ";
+	std::cin >> (*ptr).nextOfKin ;
+	std::cout << "\tDate of birth: ";
+	std::cin >> (*ptr).dOB ;
+	std::cout << "\tContact: ";
+	std::cin >> (*ptr).contact ;
+	std::cout << "\tSub county: ";
+	std::cin >> (*ptr).subCounty ;
+	std::cout << "\tGames: ";
+	std::cin >> (*ptr).games ;
+	std::cout << "\tWeight: ";
+	std::cin >> (*ptr).weight ;
+	std::cout << "\tHeight: ";
+	std::cin >> (*ptr).height ;
+	std::cout << "\tSpecial needs: ";
+	std::cin >> (*ptr).specialNeeds ;
+}
 
 int main()
 {
 	maringo mar;
 	mar.welcome();
 	//mar.school();
-	mar.religious();
+	//mar.religious();
+	mar.registration();
 }
