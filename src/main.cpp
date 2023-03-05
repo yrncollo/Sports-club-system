aa2388ee45d8745c4719b29b4bd386836922e9ab
yrncollo <ngenocollins599@gmail.com>
2023-03-05 20:32:53 +0300

    trasfered all the main.cpp headerfiles to main.h

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,20 @@
-#include <iostream>
-#include <string>
-#include <sys/types.h>
-#include <iomanip>
 
 #include "shop.h"
 #include "register.h"
+#include "games.h"
+#include "main.h"
 
 
 int main()
 {
 	maringo mar;
-	//shop sho;
-	mar.welcome();
-	mar.school();
-	mar.religious();
-	//mar.registration();
-	//sho.items();
+	//mar.welcome();
+	shop sho;
+	sho.items();
 	//sho.recipt();
+	//Games games;
+	//games.damages();
+	//games.damages_recipt();
+	//games.run_facilitation_fee();
 }
+
